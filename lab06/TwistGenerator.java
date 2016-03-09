45ff6c75ccd563d2473fda77e5f8715c71897cae
smr218 <smr218@lehigh.edu>
2016-03-09 02:25:08 +0000

    lab06

---
+++ lab06/TwistGenerator.java
@@ -0,0 +1,66 @@
+//import scanner
+import java.util.Scanner;
+//TwistGenerator class
+public class TwistGenerator  //main method
+{ 
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner( System.in );
+        int check = 0;
+        while( check == 0 ) 
+        {
+            System.out.println("Please enter a postive integer to indicate length of twist");
+            if( input.hasNextInt() )
+            {
+                int length = input.nextInt();
+                if( length > 0 ) 
+                {
+                    check++;
+                    int cross = (length - 1) / 3;
+                    int slash = length;//(2*cross) + 1;
+                    int count1 = 0;
+                    int count2 = 0;
+                    int count3 = 0;
+                    while(count1 < slash)
+                    {
+                        System.out.print("\\ ");
+                        count1+=2;
+                        if ( count1 < slash )
+                        {
+                            System.out.print("/");
+                            count1++;
+                            
+                        }// end of if statment 
+                    }// end of while loop 
+                    System.out.println("");
+                    while( count2 < cross )
+                    {
+                        System.out.print(" X ");
+                        count2++;
+                    }//end of while loop 
+                    System.out.println("");
+                    while( count3 < slash )
+                    {
+                        System.out.print("/ ");
+                        count3+=2;
+                        if (count3 < slash)
+                        {
+                            System.out.print("\\");
+                            count3++;
+                            
+                        }//end of if statement 
+                    }//end of while loop
+                }// end of if statement 
+                else
+                {
+                    System.out.println("You did not enter a postive integer");
+                }
+            }// end of if statement 
+            else
+            {
+                System.out.println("You did not enter an integer");
+                input.next();
+            }//end of else statement
+        }// end of while loop 
+    }// end of main method
+}// end of class
--- practice/practice.java
+++ practice/practice.java
@@ -3,8 +3,36 @@ public class practice
 { 
     public static void main (String [] args)
     {
-        float y = (float) 3.3;
+        /*Scanner input = new Scanner(System.in);
         int x = (int)(Math.random()*4)+1;
         System.out.println(x);
-    }
+        String s = "fall", t = "break";
+        double w, y =3.2;
+        w = 3.2;
+        */
+        
+        String s = "fall";
+        String t = "break";
+        String cont = s + t;
+        
+        boolean test = ((s + t) == cont);
+        
+        System.out.println(test);
+        System.out.println(cont.equals("fallbreak"));
+        
+        double x = 1/2.0;
+        System.out.println(x);
+
+        
+        int i = 1;
+        float j = 1;
+        
+        i++;
+        ++j;
+        
+        System.out.println(i);
+        System.out.printf(" you owe %10.2f\n" , j);
+        int y = (int)(Math.random()*4)+1;
+        System.out.println(y)
+;    }
 }
